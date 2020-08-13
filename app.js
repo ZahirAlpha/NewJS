b148107255bea78b1678c36062552fc1e5b029e8
Zahir Rafiq <cresarts@hotmail.com>
2020-08-13 10:17:29 +0100

    app js file changed vars subject

---
+++ app.js
@@ -0,0 +1,51 @@
+//===== var, let, const======
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// greeting = ('Hello');
+// console.log(greeting);
+
+// var can only include letter, numbers, _, $ 
+// Can not start with numbers 
+
+
+// Multi word vars
+
+// var firstName = 'John';
+// var first_Name = 'Sara';
+// var FirstName = 'Tom';
+
+//LET is similar to Var
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// CONST cannot be changed
+
+// const name = 'John';
+// console.log(name);
+// Can not reassign 
+// name = 'Sara';
+// Have to assign a value 
+// const greeting;
+
+// const person = {
+//   name: 'John',
+//   age: 30
+// }
+
+// person.name = 'Sara';
+// person.age = 32;
+
+// console.log(person);
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.push(6);
+// console.log(numbers);
