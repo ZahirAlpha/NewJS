9fe926dc60f67dc77ee730582ffbc64c3f125909
Zahir Rafiq <cresarts@hotmail.com>
2020-08-24 12:52:13 +0100

    JS Object Literals

--- app.js
+++ app.js
@@ -334,3 +334,54 @@
 
 // console.log(numbers);
 // console.log(val);
+
+
+// ==== Object Literals ====//
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 36,
+  email: 'steve@aol.com',
+  hobbies: ['Music', 'sport', ],
+  address: {
+    city: 'Miami',
+    state: 'FL'
+  },
+  getBirthYear: function () {
+    return 2020 - this.age;
+  }
+}
+
+let val;
+
+val = person;
+
+// get specific value
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
+
+console.log(val);
+
+const people = [{
+    name: 'John',
+    age: 30
+  },
+  {
+    name: 'Mike',
+    age: 23
+  }, {
+    name: 'Nancy',
+    age: 40
+  }
+
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
