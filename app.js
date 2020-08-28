8e82898ce0e471ce04667f14e38e1c43aff0c32d
Zahir Rafiq <cresarts@hotmail.com>
2020-08-28 15:28:57 +0100

    Window Object JS

--- app.js
+++ app.js
@@ -695,3 +695,69 @@
 // for (let x in user) {
 //   console.log(`${x} : ${user[x]}`);
 // }
+
+
+// ==== Window Objects ==== //
+
+// WIDOW METHODS / OBJECTS / PROPERTIES 
+
+// Alert 
+// alert('Hello world');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if (confirm('Are you sure?')) {
+//   console.log('YES');
+// } else {
+//   console.log('No');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points 
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect 
+//window.location.href = 'https://google.com';
+// Reload
+// window.location.reload();
+
+// History
+
+// window.history.go(-2);
+// val = window.history.length;
+
+// Navigator
+
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.languages;
+
+
+
+
+console.log(val);
